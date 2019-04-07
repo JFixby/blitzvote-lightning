4ee59a3e7ca2209efbcacf947032ff4daac5452f
J Fixby <github@jfixby.com>
2019-04-07 19:29:52 +0200

    print to console

--- blitzvote-backend/app.js
+++ blitzvote-backend/app.js
@@ -10,15 +10,16 @@ _.get('/invoices', getMessage);
 _.post('/invoices', postMessage);
 
 function getMessage(ctx) {
-  console.log("start web server");
-  
-  var grpc = new Grpc()
-  var ballance = grpc.get_wallet_balance()
-  return json(ballance)
+    console.log("getMessage");
+
+    var grpc = new Grpc()
+    var ballance = grpc.get_wallet_balance()
+    console.log("ballance: " + JSON.stringify(ballance));
+    return json(ballance)
 }
 
 function postMessage() {
-  this.body = "You just called the post method at '/hello'!\n";
+    this.body = "You just called the post method at '/hello'!\n";
 };
 app.use(json())
 app.use(_.routes()); //Use the routes defined using the router
