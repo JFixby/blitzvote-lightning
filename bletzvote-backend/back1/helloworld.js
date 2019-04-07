f8e19cd67ed72d8cfb947850ca56e3e4bdfd7e95
J Fixby <github@jfixby.com>
2019-04-07 18:18:29 +0200

    push

--- bletzvote-backend/back1/helloworld.js
+++ bletzvote-backend/back1/helloworld.js
@@ -1,8 +1,15 @@
+import { GrpcCaller } from './grpc/GrpcCaller.js'
+
+var grpc_caller = new GrpcCaller()
+
 var http = require('http');
 
 http.createServer(function (req, res) {
-  // add a HTTP header:
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('Hello World!');
-  res.end();
+    // add a HTTP header:
+    var res = grpc_caller.get_wallet_balance()
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('Hello World!');
+    res.write(res + '')
+    res.end();
 }).listen(8080);
