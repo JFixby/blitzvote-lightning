21c5f4fb3ddb51a79def24f13c2586bf4c9c10d1
Aleksandr Pilgun <alexandr.pilgun@gmail.com>
2019-04-07 18:51:14 +0200

    comit

--- blitzvote-backend/app.js
+++ blitzvote-backend/app.js
@@ -1,14 +1,17 @@
-'use strict'
 var koa = require('koa');
 var router = require('koa-router');
-var app = new koa();
+var app =  new koa();
 
-var _ = router();              //Instantiate the router
-_.get('/hello', getMessage);   // Define routes
+var _ = router(); //Instantiate the router
 
-function *getMessage() {
-   this.body = "Hello world!";
+_.get('/invoices', getMessage);
+_.post('/invoices', postMessage);
+
+function getMessage(ctx) {
+  ctx.body = "Hello world!";
+}
+function postMessage() {
+   this.body = "You just called the post method at '/hello'!\n";
 };
-
-app.use(_.routes());           //Use the routes defined using the router
+app.use(_.routes()); //Use the routes defined using the router
 app.listen(3000);
