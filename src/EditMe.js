b201349a2a6b8911aaa8f32ccee5ced81a850dc0
Aleksandr Pilgun <alexandr.pilgun@gmail.com>
2019-04-07 12:49:44 +0200

    small ui changes

--- src/EditMe.js
+++ src/EditMe.js
@@ -18,13 +18,13 @@ class EditMe extends Component {
 
   selectAnimal(e, animal) {
     e.preventDefault()
-    this.setState({selectedAnimal: true})
+    this.setState({ selectedAnimal: true })
     this.props.saveMe(Object.assign({}, this.props.me, { animal }))
   }
 
   selectTerritory(e, territory) {
     e.preventDefault()
-    this.setState({selectedTerritory: true})
+    this.setState({ selectedTerritory: true })
     this.props.saveMe(Object.assign({}, this.props.me, { territory }))
   }
 
@@ -43,27 +43,27 @@ class EditMe extends Component {
 
     return (
       <div className="EditMe container">
-      <h2>Your animal persona</h2>
-      <p>You are the ruler of your kingdom! Pick your animal persona and its home!</p>
+        {/* <h2>Your animal persona</h2>
+        <p>You are the ruler of your kingdom! Pick your animal persona and its home!</p>
         <h3>Select your animal</h3>
         <div className="row card-deck">
-        { ANIMALS.map((animal, index) => {
-          const selected = myAnimal && myAnimal.id === animal.id
-          return (
-            <Card path='/animals/' key={index} item={animal} select={this.selectAnimal} selected={selected} />
-                )
+          {ANIMALS.map((animal, index) => {
+            const selected = myAnimal && myAnimal.id === animal.id
+            return (
+              <Card path='/animals/' key={index} item={animal} select={this.selectAnimal} selected={selected} />
+            )
           })
-        }
-        </div>
-        <h3 className="select-territory">Select your territory</h3>
+          }
+        </div> */}
+        <h3 className="select-territory">Vote for you candidate</h3>
         <div className="card-deck">
-        { TERRITORIES.map((territory, index) => {
-          const selected = myTerritory && myTerritory.id === territory.id
-          return (
-            <Card path='/territories/' key={index} item={territory} select={this.selectTerritory} selected={selected} />
-                )
+          {TERRITORIES.map((territory, index) => {
+            const selected = myTerritory && myTerritory.id === territory.id
+            return (
+              <Card path='/territories/' key={index} item={territory} select={this.selectTerritory} selected={selected} />
+            )
           })
-        }
+          }
         </div>
         <div className="container row">
           <div className="col-lg-12 done">
