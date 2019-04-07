f0c402563e820beb828fd1f205453aba46806057
J Fixby <github@jfixby.com>
2019-04-07 19:41:13 +0200

    backend 2

--- bletzvote-backend/back2/data.js
+++ bletzvote-backend/back2/data.js
@@ -1,20 +1,39 @@
 // data.js
-import GrpcCaller from './GrpcCaller.js'
 
 var http = require('http');
+var fs = require('fs');
+var grpc = require('grpc');
+var lnrpc = grpc.load('rpc.proto').lnrpc;
+process.env.GRPC_SSL_CIPHER_SUITES = 'HIGH+ECDSA'
+var lndCert = fs.readFileSync('tls.cert');
+var sslCreds = grpc.credentials.createSsl(lndCert);
+var macaroonCreds = grpc.credentials.createFromMetadataGenerator(function (args, callback) {
+    var macaroon = fs.readFileSync("admin.macaroon").toString('hex');
+    var metadata = new grpc.Metadata()
+    metadata.add('macaroon', macaroon);
+    callback(null, metadata);
+});
+var creds = grpc.credentials.combineChannelCredentials(sslCreds, macaroonCreds);
+var lightning = new lnrpc.Lightning('[2001:a18:a:c3::11]:10009', creds);
+
 
 export const add = (x, y) => {
     console.log("start web server");
-    var grpc_caller = new GrpcCaller()
     // grpc_caller.get_wallet_balance()
     http.createServer(function (req, res) {
         // add a HTTP header:
         // var res = grpc_caller.get_wallet_balance()
-        var ballance = grpc_caller.get_wallet_balance()
+
+        var request = {}
+        var balance = lightning.walletBalance(request, function (err, response) {
+            console.log(response);
+            console.log(err);
+        })
+
 
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write('Hello World!');
-        res.write(JSON.stringify(ballance))
+        res.write(JSON.stringify(balance))
         res.end();
     }).listen(8080);
 
